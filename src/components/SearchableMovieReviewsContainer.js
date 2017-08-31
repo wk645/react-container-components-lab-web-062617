3fce51a8c3fb8c1856ec2a31d2e09736ee508ee6
wk645 <wk645@nyu.edu>
2017-08-31 09:04:02 -0400

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
-
 export default class SearchableMovieReviewsContainer extends Component {
 
 	constructor() {
